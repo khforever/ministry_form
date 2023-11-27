c7c91d3a431d54185960650ae7f2d634a8260ca6
khforever <59407743+khforever@users.noreply.github.com>
2023-11-27 08:01:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+<!-- 
+<style>
+body{
+  background-color:rosybrown;
+  
+}
+
+form{
+  background-color:rosybrown;
+  
+}
+  </style> -->
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <!-- Font Awesome -->
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    /> -->
+    <!-- Google Fonts Roboto -->
+    
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+    />
+    <!-- MDB -->
+
+
+
+    
+    <link rel="stylesheet" href="css/mdb.min.css" />
+  
+  
+  
+  
+  </head>
+  <body>
+<!-- start code form -->
+
+
+
+
+
+
+
+
+
+
+<div class="container my-5 py-5">
+
+  <!--Section: Design Block-->
+  <section>
+
+    <div class="row">
+      <div class="col-md-8">
+        <div class="card mb-4">
+
+
+        </div>
+    
+
+      </div>
+
+
+
+
+
+
+
+     
+
+      <div class="col-md-8 mb-4">
+        <div class="card mb-4">
+          <div class="card-header py-3">
+            <h5 class="mb-0 text-font text-uppercase">Delivery address</h5>
+          </div>
+          <div class="card-body">
+
+          <?php
+
+
+?>
+            <form>
+
+              <div class="row mb-4">
+                <div class="col">
+                  <div class="form-outline">
+                    <input type="text" id="form11Example1" class="form-control" />
+                    <label class="form-label" for="form11Example1">First name</label>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-outline">
+                    <input type="text" id="form11Example2" class="form-control" />
+                    <label class="form-label" for="form11Example2">Last name</label>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Text input -->
+              <div class="form-outline mb-4">
+                <input type="text" id="form11Example3" class="form-control" />
+                <label class="form-label" for="form11Example3">Company name</label>
+              </div>
+
+              <!-- Text input -->
+              <div class="form-outline mb-4">
+                <input type="text" id="form11Example4" class="form-control" />
+                <label class="form-label" for="form11Example4">Address</label>
+              </div>
+
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" id="form11Example5" class="form-control" />
+                <label class="form-label" for="form11Example5">Email</label>
+              </div>
+
+              <!-- Number input -->
+              <div class="form-outline mb-4">
+                <input type="number" id="form11Example6" class="form-control" />
+                <label class="form-label" for="form11Example6">Phone</label>
+              </div>
+
+              <!-- Message input -->
+              <div class="form-outline mb-4">
+                <textarea class="form-control" id="form11Example7" rows="4"></textarea>
+                <label class="form-label" for="form11Example7">Additional information</label>
+              </div>
+
+              <!-- Checkbox -->
+              <div class="form-check d-flex justify-content-center mb-2">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form11Example8" checked />
+                <label class="form-check-label" for="form11Example8">
+                  Create an account?
+                </label>
+              </div>
+            </form>
+          </div>
+
+
+
+
+          
+        </div>
+
+        
+        <div class="text-center">
+          <button type="button" class="btn button-order col-md-10">Place order</button>
+        </div>
+
+      </div>
+
+
+
+
+
+
+    </div>
+
+  </section>
+  <!--Section: Design Block-->
+
+</div>
+
+
+
+
+
+
+<!-- ooooooooooooooooooooooooooo -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Start your project here-->
+
+    <!-- End your project here-->
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
